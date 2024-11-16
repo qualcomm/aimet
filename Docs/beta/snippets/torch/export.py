3fb4085a094357a0dc0523da27a3e9f6df4dea07
Dave Welsch <dwelsch@expertsupport.com>
2024-11-16 11:27:35 -0800

    Fix TOC to display 2 levels, no collapse.
    Signed-off-by: Dave Welsch <dwelsch@expertsupport.com>

--- Docs/beta/snippets/torch/export.py
+++ Docs/beta/snippets/torch/export.py
@@ -36,7 +36,7 @@
 # =============================================================================
 # pylint: disable=missing-docstring
 
-    # Export the model
-    # Export the model which saves pytorch model without any simulation nodes and saves encodings file for both
-    # activations and parameters in JSON format
-    model.export(path='./', filename_prefix='<name_prefix>', dummy_input=dummy_input.cpu())
+# Export the model
+# Export the model which saves pytorch model without any simulation nodes and saves encodings file for both
+# activations and parameters in JSON format
+model.export(path='./', filename_prefix='<name_prefix>', dummy_input=dummy_input.cpu())
