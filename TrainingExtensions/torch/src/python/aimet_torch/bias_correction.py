cf4cdb953b817b626ec2311a320798750df03113
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2024-12-10 19:09:59 -0800

    Fix import statement
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/bias_correction.py
+++ TrainingExtensions/torch/src/python/aimet_torch/bias_correction.py
@@ -56,7 +56,7 @@ from aimet_common.bias_correction import (
 from aimet_common.defs import ActivationType
 
 from aimet_torch import utils
-from aimet_torch import quantsim as qsim
+from aimet_torch.v1 import quantsim as qsim
 from aimet_torch.meta.connectedgraph import ConnectedGraph
 from aimet_torch.save_utils import SaveUtils
 from aimet_torch.utils import get_ordered_lists_of_conv_fc
