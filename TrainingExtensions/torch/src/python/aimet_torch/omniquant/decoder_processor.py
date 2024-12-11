f1d5a51bd6f0285298527953021d5c9ce8a52ee4
Mu-Chien Hsu <quic_muchhsu@quicinc.com>
2024-12-10 18:01:34 -0800

    Initial commit for aimet_torch.omniquant: OMQ init commit, config, get transformer blocks, get let pairs.
    Signed-off-by: Mu-Chien Hsu <quic_muchhsu@quicinc.com>

---
+++ TrainingExtensions/torch/src/python/aimet_torch/omniquant/decoder_processor.py
@@ -0,0 +1,107 @@
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+""" Process transformer models to get decoder list and LET pair modules for supporting models only """
+from abc import ABC, abstractmethod
+from transformers import LlamaModel, LlamaForCausalLM
+import torch
+
+from .defs import _LetPair
+
+LlamaModelGroup = (LlamaModel, LlamaForCausalLM)
+
+def get_transformer_processor(model):
+    """ Return transformer_processor based on model class family. """
+    if isinstance(model, LlamaModelGroup):
+        return LlamaProcessor
+
+    def _get_supporting_model_class():
+        """ Helping function to pretty print supporting model classes. """
+        model_class_str = ""
+        for model_class in LlamaModelGroup:
+            model_class_str += model_class.__name__
+            model_class_str += ", "
+        return model_class_str[:-2]
+
+    raise ValueError(f"AIMET Omniquant only support class: {_get_supporting_model_class()} from transformer package,\
+but got class {model.__class__}")
+
+# pylint: disable=unnecessary-pass
+class TransformerProcessor(ABC):
+    """ Abstract class for transformer processors. """
+    @classmethod
+    @abstractmethod
+    def get_decoder_list(cls, model):
+        """ Method to get decoder module list. """
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_let_module_pair(cls, decoder_block):
+        """ Method to get a list of let module pairs in a decoder_block. """
+        pass
+
+class LlamaProcessor(TransformerProcessor):
+    """ Transformer Procesor for LlamaModelGroup = (LlamaModel, LlamaForCausalLM) """
+    @classmethod
+    def get_decoder_list(cls, model)->torch.nn.ModuleList:
+        """ Method to get decoder module list. """
+        model = getattr(model, "model", model)
+        transformer_block_list = model.get_submodule("layers")
+
+        assert isinstance(transformer_block_list, torch.nn.ModuleList), \
+            f"transformer_block_list: {transformer_block_list} is not a ModuleList"
+        return transformer_block_list
+
+    @classmethod
+    def get_let_module_pair(cls, decoder_block) -> list:
+        """ Method to get a list of let module pairs in a decoder_block. """
+        input_layernorm = decoder_block.get_submodule("input_layernorm")
+        q_proj = decoder_block.get_submodule("self_attn.q_proj")
+        k_proj = decoder_block.get_submodule("self_attn.k_proj")
+        v_proj = decoder_block.get_submodule("self_attn.v_proj")
+        o_proj = decoder_block.get_submodule("self_attn.o_proj")
+        gate_proj = decoder_block.get_submodule("mlp.gate_proj")
+        up_proj = decoder_block.get_submodule("mlp.up_proj")
+        down_proj = decoder_block.get_submodule("mlp.down_proj")
+        output_layernorm = decoder_block.get_submodule("post_attention_layernorm")
+
+        return [
+            _LetPair([input_layernorm], [q_proj, k_proj, v_proj]),
+            _LetPair([v_proj], [o_proj]),
+            _LetPair([output_layernorm], [gate_proj, up_proj]),
+            _LetPair([up_proj], [down_proj])
+            ]
---
+++ TrainingExtensions/torch/src/python/aimet_torch/omniquant/omniquant_optimizer.py
@@ -0,0 +1,110 @@
+# -*- mode: python -*-
+# =============================================================================
+#  @@-COPYRIGHT-START-@@
+#
+#  Copyright (c) 2025, Qualcomm Innovation Center, Inc. All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions are met:
+#
+#  1. Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#  2. Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#  3. Neither the name of the copyright holder nor the names of its contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+#  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+#  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+#  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+#  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+#  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+#  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+#  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+#  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+#
+#  SPDX-License-Identifier: BSD-3-Clause
+#
+#  @@-COPYRIGHT-END-@@
+# =============================================================================
+""" Optimizer for Omniquant """
+
+import torch
+
+from aimet_torch.v2.quantsim import QuantizationSimModel
+
+from .decoder_processor import get_transformer_processor
+from .omniquant_config import OmniquantConfig
+
+class Omniquant:
+    """
+    Omniqunat for Post Training Quantization (PTQ)
+    """
+    @classmethod
+    def apply_omniquant(cls, quant_sim: QuantizationSimModel, model: torch.nn.Module, omniquant_config: OmniquantConfig, dataloader,
+                        output_path: str) -> torch.nn.Module:
+        """
+        Returns model with with omniquant weight, and save model with new weightsto output_path.
+
+        :param quant_sim: QuantizationSimModel object to optimize with Omniquant.
+        :param model: Original fp32 model from which quant_sim was created.
+        :param omniquant_config: Configuration for Omniquant optimization.
+        :param dataloader: Dataloader used to train model.
+        :param output_path: path where to store artifacts.
+        :return: Model with Omniquant weights.
+        """
+        quant_sim.model = cls._apply_omniquant(quant_sim, model, omniquant_config, dataloader, output_path)
+        return quant_sim.model
+
+    # pylint: disable=too-many-locals
+    # pylint: disable=unused-variable
+    # pylint: disable=unused-argument
+    @classmethod
+    def _apply_omniquant(cls, quant_sim: QuantizationSimModel, model: torch.nn.Module, omniquant_config: OmniquantConfig,
+                         dataloader, output_path: str) -> torch.nn.Module:
+        """
+        Implemenatation to run omniquant optimization block by block. Return model with optimized weights.
+
+        :param quant_sim: QuantizationSimModel object to optimize with Omniquant.
+        :param model: Original fp32 model from which quant_sim was created.
+        :param omniquant_config: Configuration for Omniquant optimization.
+        :param dataloader: Dataloader used to train model.
+        :param output_path: path where to store artifacts.
+        :return: Model with Omniquant weights.
+        """
+        transformer_processor = get_transformer_processor(model)
+        fp_transformer_block_list = transformer_processor.get_decoder_list(model)
+        qt_transformer_block_list = transformer_processor.get_decoder_list(quant_sim.model)
+
+        def calibration_wrapper(model, dataloader):
+            for batch_id, batch in enumerate(dataloader):
+                if batch_id < omniquant_config.num_batch:
+                    batch = tuple(batch) # Dataloader returns List[torch.Tensor]
+                    model(*batch)
+                else:
+                    break
+
+        quant_sim.compute_encodings(calibration_wrapper, dataloader)
+
+        for block_num, (fp_block, qt_block) in enumerate(zip(fp_transformer_block_list, qt_transformer_block_list)):
+            fp_let_pair_list = transformer_processor.get_let_module_pair(fp_block)
+            qt_let_pair_list = transformer_processor.get_let_module_pair(qt_block)
+
+            for epoch in range(omniquant_config.num_epoch):
+                pass
+
+        # pylint: disable=protected-access
+        # QDQ on models to fold quantizations into weight params.
+        quant_sim._apply_qdq_to_model_parameters(quant_sim.model)
+        # pylint: disable=unnecessary-comprehension
+        all_modules_in_original_model = [module for module in quant_sim.model.modules()]
+        quant_sim._remove_quantization_wrappers(quant_sim.model, all_modules_in_original_model)
+
+        return quant_sim.model
