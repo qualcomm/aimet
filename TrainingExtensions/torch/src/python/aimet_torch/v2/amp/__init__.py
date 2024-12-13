73be39ff61f139c7618d20afea2071dc9e7a68e5
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2024-12-12 18:55:33 -0800

    Fix copyright string
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/v2/amp/__init__.py
+++ TrainingExtensions/torch/src/python/aimet_torch/v2/amp/__init__.py
@@ -30,6 +30,8 @@
 #  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 #  POSSIBILITY OF SUCH DAMAGE.
 #
+#  SPDX-License-Identifier: BSD-3-Clause
+#
 #  @@-COPYRIGHT-END-@@
 # =============================================================================
 """ Mixed precision inference """
