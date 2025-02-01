4cf566295372bffd45b4fef7bf4d75e8879c8cd7
Kyunggeun Lee <quic_kyunggeu@quicinc.com>
2025-02-01 08:29:04 -0800

    Resolve consider-using-with warning
    Signed-off-by: Kyunggeun Lee <quic_kyunggeu@quicinc.com>

--- TrainingExtensions/torch/src/python/aimet_torch/_base/adaround/adaround_weight.py
+++ TrainingExtensions/torch/src/python/aimet_torch/_base/adaround/adaround_weight.py
@@ -229,7 +229,8 @@ class AdaroundBase(ABC):
             # AdaRound must be applied to modules in the order of occurrence
             if checkpoints_config:
                 # Load the predefined json file for checkpoints info
-                checkpoint_config = json.load(open(checkpoints_config))
+                with open(checkpoints_config) as f:
+                    checkpoint_config = json.load(f)
                 convert_configs_values_to_bool(checkpoint_config)
 
                 assert 'cache_on_cpu' in checkpoint_config.keys(), \
