f71f89874e5f19273305df64ff4f2040659f041f
Kumar, Ashvin <ashvkuma@qti.qualcomm.com>
2026-05-18 12:44:15 -0700

    Change QuantSim alias (#7088)
    Signed-off-by: Ashvin Kumar <quic_ashvkuma@quicinc.com>
    Co-authored-by: Ashvin Kumar <quic_ashvkuma@quicinc.com>

--- GenAILab/qai_hub_lm/backends/__init__.py
+++ GenAILab/qai_hub_lm/backends/__init__.py
@@ -1,4 +1,4 @@
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause
 
-QUANTSIM_CONFIG = "htp_quantsim_config_v73_per_channel_linear.json"
+QUANTSIM_CONFIG = "htp_v73"
